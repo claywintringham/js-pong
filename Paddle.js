1d4cdc9be20e5f8411b7ae7ea65e48a6403b41f8
Aiken Wintringham <aikenwintringham@outlook.com>
2023-05-27 11:31:36 +0800

    first commit

---
+++ Paddle.js
@@ -0,0 +1,30 @@
+const SPEED = 0.08;
+
+export default class Paddle {
+  constructor(paddleElem) {
+    this.paddleElem = paddleElem;
+    this.reset();
+  }
+
+  get position() {
+    return parseFloat(
+      getComputedStyle(this.paddleElem).getPropertyValue("--position")
+    );
+  }
+
+  set position(value) {
+    return parseFloat(this.paddleElem.style.setProperty("--position", value));
+  }
+
+  rect() {
+    return this.paddleElem.getBoundingClientRect();
+  }
+
+  reset() {
+    this.position = 50;
+  }
+
+  update(delta, ballHeight) {
+    this.position += SPEED * delta * (ballHeight - this.position); //if ball above current position, then move downwards, vice versa
+  }
+}
